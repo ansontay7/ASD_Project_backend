43abd0dc861538f0a6b1aa402498f1bcbc683221
ansontay7 <ansontnw0707@gmail.com>
2026-01-09 23:33:23 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(express.json());
 /* Routes */
 app.use('/api', inventoryRoutes);
 app.use('/api', stockRoutes);
-app.use('/api', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api', purchaseRoutes);
 app.use("/api", supplierRoutes);
 
