2aa5eed33386e96e079f7eac74903ad39a29e1da
ansontay7 <ansontnw0707@gmail.com>
2026-01-09 23:32:30 +0800

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/auth.controller');
 
-router.post('/auth/login', controller.login);
+router.post('/login', controller.login);
 
 module.exports = router;
